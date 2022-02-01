208db75ad56b9db3663425de4e7c5ade64d03bfa
Moshe <moisherosenzweig@gmail.com>
2022-02-01 16:45:51 -0500

    commit tree dcf7bd9644f42befdd51fd69258d3663bbc27624

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide = (x,y) => {
+    return 2000/ 100;
+    // return x/y;
+
+}
+
+console.log(divide(2000,100));
+
+const square = (x)=> {
+    return x*x;
+
+}
+
+const add = (x=5,y=20)=> {
+return x+y;
+}
